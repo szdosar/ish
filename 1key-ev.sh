430c841f8e7587471702144214d8a19ee3e0a8e9
szdosar <dosar@live.com>
2023-02-14 10:56:10 +0800

    Update and rename 1key-naive.sh to 1key-ev.sh

---
+++ 1key-ev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+### tmux
+timedatectl set-timezone Asia/Shanghai
+apt-get -y update
+apt-get -y install tmux
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+tmux
