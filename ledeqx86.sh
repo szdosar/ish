be6b65347ec229df91a6223c1b9e5a37b83fd93d
szdosar <dosar@live.com>
2023-03-22 14:17:18 +0800

    Update ledeqx86.sh

--- ledeqx86.sh
+++ ledeqx86.sh
@@ -12,6 +12,6 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 ./scripts/feeds update -a && ./scripts/feeds install -a && make defconfig
 rm .config
-wget https://raw.githubusercontent.com/szdosar/Actions-OpenWrt/main/.config .config
+wget https://raw.githubusercontent.com/szdosar/Actions-OpenWrt/main/.config
 ./scripts/feeds update -a -f && ./scripts/feeds install -a -f && make defconfig
 make download -j8 && make V=s -j$(nproc)
