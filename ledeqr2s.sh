6e401c3efe6aa26c7a6b2d3f9f608eb7599da4f8
szdosar <dosar@live.com>
2023-03-22 14:20:11 +0800

    Update ledeqr2s.sh

--- ledeqr2s.sh
+++ ledeqr2s.sh
@@ -5,16 +5,13 @@ sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
 git clone https://github.com/coolsnowwolf/lede
 cd lede
-make defconfig
-rm .config
-wget https://raw.githubusercontent.com/szdosar/Actions-OpenWrt-r2s/main/.config .config
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.4.1/g' package/base-files/files/bin/config_generate
-./scripts/feeds update -a -f && ./scripts/feeds install -a -f && make defconfig
+./scripts/feeds update -a && ./scripts/feeds install -a && make defconfig
 rm .config
-wget https://raw.githubusercontent.com/szdosar/Actions-OpenWrt-r2s/main/.config .config
+wget https://raw.githubusercontent.com/szdosar/Actions-OpenWrt-r2s/main/.config
 ./scripts/feeds update -a -f && ./scripts/feeds install -a -f && make defconfig
 make download -j8 && make V=s -j$(nproc)
