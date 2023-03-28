40d61f2b4ca643e7215def6ea5afda6b3fb570b0
szdosar <dosar@live.com>
2023-03-28 15:06:40 +0800

    Create 1key-go-amd-120.sh

---
+++ 1key-go-amd-120.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+### go 1.20 for amd64
+set -e -o pipefail
+curl -Lo go.tar.gz https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go.tar.gz
+rm go.tar.gz
+ln -s /usr/local/go/bin/go /usr/bin/go
