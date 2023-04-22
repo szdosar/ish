06228feeeaadc448bdf96ad8361fe6079f5176fb
szdosar <dosar@live.com>
2023-04-23 05:35:40 +0800

    Update ledeqx86v2ray.sh

--- ledeqx86v2ray.sh
+++ ledeqx86v2ray.sh
@@ -1,5 +1,5 @@
 cd
-rm -rf lede
+rm -rf ~/lede
 sudo -E apt-get -qq install $(curl -fsSL raw.githubusercontent.com/szdosar/ish/master/depends-ubuntu-2204)
 sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
@@ -11,6 +11,7 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 ./scripts/feeds update -a && ./scripts/feeds install -a && make defconfig
 rm .config
-wget https://raw.githubusercontent.com/szdosar/Actions-OpenWrt/main/.config
+wget https://raw.githubusercontent.com/szdosar/ish/master/x86.config
+mv x86.config .config
 ./scripts/feeds update -a -f && ./scripts/feeds install -a -f && make defconfig
 make download -j8 && make V=s -j$(nproc)
