284e5594b2ea92577f68e25e4edf89dcba6ca946
szdosar <dosar@live.com>
2023-09-26 16:27:41 +0800

    Update install_ffmpeg_svtav1_arm.sh

--- install_ffmpeg_svtav1_arm.sh
+++ install_ffmpeg_svtav1_arm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 进入/tmp目录
+cd /tmp
+rm -rf SVT-AV1-tmp
+mkdir -p SVT-AV1-tmp && cd SVT-AV1-tmp
+
 # 安装必要的依赖
 sudo apt-get update
 sudo apt-get install -y build-essential git yasm pkg-config
@@ -10,16 +15,19 @@ chmod +x cmake-3.27.6-linux-aarch64.sh
 sudo ./cmake-3.27.6-linux-aarch64.sh --prefix=/usr/local --skip-license
 
 # 克隆并编译SVT-AV1
-git clone https://github.com/AOMediaCodec/SVT-AV1.git
+git clone --depth 1 https://gitlab.com/AOMediaCodec/SVT-AV1.git
 cd SVT-AV1
-git submodule update --init
-cd Build
+mkdir build && cd build
 cmake ..
 make -j$(nproc)
 sudo make install
 
 # 编译ffmpeg
-cd ~
+cd /tmp
+rm -rf SVT-AV1-tmp
+rm -rf ffmpeg-tmp
+mkdir -p ffmpeg-tmp && cd ffmpeg-tmp
+
 if [ ! -d "ffmpeg" ]; then
   git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
 fi
