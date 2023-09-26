d6d2a4cb516b7f579aba6bb85f1d2861620862e9
szdosar <dosar@live.com>
2023-09-26 16:41:04 +0800

    Update install_ffmpeg_svtav1_arm.sh

--- install_ffmpeg_svtav1_arm.sh
+++ install_ffmpeg_svtav1_arm.sh
@@ -9,10 +9,17 @@ mkdir -p SVT-AV1-tmp && cd SVT-AV1-tmp
 sudo apt-get update
 sudo apt-get install -y build-essential git yasm pkg-config
 
-# 下载并安装cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.27.6/cmake-3.27.6-linux-aarch64.sh
-chmod +x cmake-3.27.6-linux-aarch64.sh
-sudo ./cmake-3.27.6-linux-aarch64.sh --prefix=/usr/local --skip-license
+# 检查cmake版本
+CMAKE_VERSION=$(cmake --version 2>&1 | head -n1 | cut -d" " -f3)
+CMAKE_MAJOR=$(echo $CMAKE_VERSION | cut -d. -f1)
+CMAKE_MINOR=$(echo $CMAKE_VERSION | cut -d. -f2)
+
+if [ "$CMAKE_MAJOR" -lt "3" ] || [ "$CMAKE_MAJOR" -eq "3" -a "$CMAKE_MINOR" -lt "16" ]; then
+  # 下载并安装cmake
+  wget https://github.com/Kitware/CMake/releases/download/v3.27.6/cmake-3.27.6-linux-aarch64.sh
+  chmod +x cmake-3.27.6-linux-aarch64.sh
+  sudo ./cmake-3.27.6-linux-aarch64.sh --prefix=/usr/local --skip-license
+fi
 
 # 克隆并编译SVT-AV1
 git clone --depth 1 https://gitlab.com/AOMediaCodec/SVT-AV1.git
