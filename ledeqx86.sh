2fcce49ad9441917a62763ed433b0eb5fd333795
szdosar <dosar@live.com>
2024-01-02 08:30:46 +0800

    Update ledeqx86.sh

--- ledeqx86.sh
+++ ledeqx86.sh
@@ -5,10 +5,10 @@ sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
 git clone https://github.com/coolsnowwolf/lede
 cd lede
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
+sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;main' feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 ./scripts/feeds update -a && ./scripts/feeds install -a && make defconfig
 rm .config
