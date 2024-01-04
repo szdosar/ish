1bc5249ea32252daeee95b699c70641e10bad92a
szdosar <dosar@live.com>
2024-01-04 08:45:56 +0800

    Update ledeqx86.sh

--- ledeqx86.sh
+++ ledeqx86.sh
@@ -5,6 +5,7 @@ sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
 git clone https://github.com/coolsnowwolf/lede
 cd lede
+git reset --hard 9d124b993644b4f77749788936c44ca0c884f184
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
 sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;main' feeds.conf.default
