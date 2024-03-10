ee47780506c140b7b97c93acd01fc016e52ada39
szdosar <dosar@live.com>
2024-03-10 09:15:11 +0800

    Update 1key-naive.sh

--- 1key-naive.sh
+++ 1key-naive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-### go 1.20 for amd64
+### go 1.22 for amd64
 set -e -o pipefail
-curl -Lo go.tar.gz https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
+curl -Lo go.tar.gz https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go.tar.gz
 rm go.tar.gz
