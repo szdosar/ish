6413206a623b1461e888f40b6b4da604249b9f8d
szdosar <dosar@live.com>
2024-03-10 11:43:26 +0800

    Create 1key-go-amd-1913.sh

---
+++ 1key-go-amd-1913.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+### go 1.19 for amd64
+set -e -o pipefail
+curl -Lo go.tar.gz https://go.dev/dl/go1.19.13.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go.tar.gz
+rm go.tar.gz
+sudo ln -s /usr/local/go/bin/go /usr/bin/go
