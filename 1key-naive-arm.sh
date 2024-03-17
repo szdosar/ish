bc1a41b4a824c1cbddaaac987738c652654793af
szdosar <dosar@live.com>
2024-03-17 18:32:40 +0800

    Update 1key-naive-arm.sh

--- 1key-naive-arm.sh
+++ 1key-naive-arm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-### go 1.20 for arm64
+### go 1.22.1 for arm64
+sudo apt update -y && sudo apt upgrade -y
 set -e -o pipefail
-curl -Lo go.tar.gz https://go.dev/dl/go1.20.1.linux-arm64.tar.gz
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go.tar.gz
+curl -Lo go.tar.gz https://go.dev/dl/go1.22.1.linux-arm64.tar.gz
+sudo rm -rf /usr/bin/go && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz
 rm go.tar.gz
 ln -s /usr/local/go/bin/go /usr/bin/go
 
