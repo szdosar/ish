7f9ec5b45dcb2edd0674d400f8fcb7632325b799
szdosar <dosar@live.com>
2024-03-17 18:21:21 +0800

    Update 1key-naive.sh

--- 1key-naive.sh
+++ 1key-naive.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-### go 1.19.13 for amd64
+### go 1.22.1 for amd64
 sudo apt update -y && sudo apt upgrade -y
 set -e -o pipefail
 curl -Lo go.tar.gz https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
 sudo rm -rf /usr/bin/go && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz
 rm go.tar.gz
-ln -s /usr/local/go/bin/go /usr/bin/go
+### ln -s /usr/local/go/bin/go /usr/bin/go
 
 ### caddy for naive
 go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
