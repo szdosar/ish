fd0dc234c22b3ee3cb70c059c32405f1a74e7d7e
szdosar <dosar@live.com>
2024-07-23 16:47:33 +0800

    Update clean_space.sh

--- clean_space.sh
+++ clean_space.sh
@@ -2,6 +2,10 @@
 
 echo "开始清理Ubuntu 22.04 LTS服务器版的磁盘空间..."
 
+# 显示清理前的磁盘空间使用情况
+echo "清理前的磁盘空间使用情况："
+df -h
+
 # 更新包列表
 sudo apt update
 
@@ -26,6 +30,10 @@ sudo rm -rf /var/tmp/*
 
 # 删除过期的快照（如果使用了snap）
 sudo snap set system refresh.retain=2
-sudo snap remove --purge $(snap list --all | awk '/disabled/{print $1, $3}' | awk '{print $1"="$2}')
+sudo snap list --all | awk '/disabled/{print $1, $3}' | while read snapname revision; do sudo snap remove "$snapname" --revision="$revision"; done
+
+# 显示清理后的磁盘空间使用情况
+echo "清理后的磁盘空间使用情况："
+df -h
 
 echo "清理完成！"
