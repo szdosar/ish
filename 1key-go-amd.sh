48602dce9f4374cd4dc03110489aba63224bca7d
szdosar <dosar@live.com>
2024-09-08 14:31:14 +0800

    Update 1key-go-amd.sh

--- 1key-go-amd.sh
+++ 1key-go-amd.sh
@@ -2,7 +2,7 @@
 ### go 1.22.5 for amd64
 set -e -o pipefail
 curl -Lo go.tar.gz https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
-sudo rm -rf /usr/bin/go
-sudo tar -C ~ -xzf go.tar.gz
-sudo mv ~/go/bin/go /usr/bin/
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go.tar.gz
 rm go.tar.gz
+sudo ln -s /usr/local/go/bin/go /usr/bin/go
